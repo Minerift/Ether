437013e0d384b8dedce5c38d66f1fbdddac4a2fd
avaerian <rblxavaerian@gmail.com>
2022-11-16 21:21:26 -0500

    Set up basic plugin

--- src/main/java/org/minerift/ether/EtherPlugin.java
+++ src/main/java/org/minerift/ether/EtherPlugin.java
@@ -1,4 +1,22 @@
 package org.minerift.ether;
 
-public class EtherPlugin {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EtherPlugin extends JavaPlugin {
+
+    private static EtherPlugin INSTANCE;
+
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+
+    }
+    @Override
+    public void onDisable() {
+
+    }
+
+    public static EtherPlugin getInstance() {
+        return INSTANCE;
+    }
 }
